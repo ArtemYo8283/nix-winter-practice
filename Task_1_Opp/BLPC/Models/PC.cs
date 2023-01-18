645e7b67daff0ca343c2df895eee95c604aa60d9
Artem <artemwot90@gmail.com>
2023-01-18 02:21:33 +0200

    Task1

---
+++ Task_1_Opp/BLPC/Models/PC.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLPC.Models
+{
+    public class PC {
+        public PC() {
+
+        }
+        public PC(int Id, string Name, float Price) { 
+            this.Id = Id;
+            this.Name = Name;
+            this.Price = Price;
+        }
+
+        private int _id;
+        private string? _name;
+        private float _price;
+
+        public int Id { 
+            get { return _id; } 
+            set { _id = value; }
+        }
+        public string? Name {
+            get { return _name; }
+            set
+            {
+                if (String.IsNullOrEmpty(value)) {
+                    throw new ArgumentNullException("Name is incorrect");
+                }
+                else {
+                    _name = value;
+                }
+            }
+        }
+
+        public float Price {
+            get { return _price; }
+            set {
+                if (value <= 0 || value == null) {
+                    throw new ArgumentNullException("Price is incorrect");
+                } else {
+                    _price = value;
+                }
+            } 
+        }
+        private bool IsBooked { get; set; }
+
+        public PC search()
+        {
+
+            return new PC();
+        }
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
---
+++ Task_1_Opp/PreasantionPC/Program.cs
@@ -0,0 +1,74 @@
+﻿using BLPC.Models;
+
+namespace PreasantionPC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu = -1;
+            int N = 0;
+            while (true) {
+                Console.WriteLine("1 - Add PC:");
+                N = Convert.ToInt32(Console.ReadLine());
+                if (N > 0) {
+                    break;
+                }
+            }
+            do
+            {
+                Console.WriteLine("1 - Add PC\n"+
+                    "2 - View all PC\n" +
+                    "3 - Search PC\n" +
+                    "4 - Delete PC\n" +
+                    "5 - Add PC\n" +
+                    "0 - Exit\n");
+
+                menu = Convert.ToInt32(Console.ReadLine());
+                switch (menu) {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+                        int menu1 = -1;
+                        do
+                        {
+                            Console.WriteLine("1 - By Id\n" +
+                                "2 - By Name\n" +
+                                "3 - Price from\n" +
+                                "4 - Price up to\n" +
+                                "0 - Exit\n");
+
+                            menu1 = Convert.ToInt32(Console.ReadLine());
+                            switch (menu1)
+                            {
+                                case 1:
+
+                                    break;
+                                case 2:
+
+                                    break;
+                                case 3:
+
+                                    break;
+                                case 4:
+
+                                    break;
+                            }
+                        } while (menu1 != 0);
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+
+                }
+            } while (menu != 0);
+        }
+    }
+}
